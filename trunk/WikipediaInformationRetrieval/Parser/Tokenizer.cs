d7bc466a399d8d12698f2f54b3302c81147eef25
szymek.fogiel <szymek.fogiel@43718346-3035-11de-94d6-134fa40061eb>
2009-05-01 11:37:43 +0000

    Added Lematizer and updated Tokenizer and WordsStream

--- trunk/WikipediaInformationRetrieval/Parser/Tokenizer.cs
+++ trunk/WikipediaInformationRetrieval/Parser/Tokenizer.cs
@@ -11,16 +11,24 @@ namespace Parser
     /// It turns list of strings into tokens, that is 
     /// different list of strings.
     /// </summary>
-    public class Tokenizer : StringConverter
+    public class Tokenizer
     {
         /// <summary>
         /// Converts list of strings into another list of strings (tokens).
         /// </summary>
         /// <param name="strings">List of strings to convert</param>
         /// <returns>Converted list of strings</returns>
-        public override List<string> ConvertStrings(List<string> strings)
+        public List<string> ConvertStrings(List<string> strings)
         {
-            return strings;
+            List<string> list = new List<string>();
+            char[] trim_chars = { ' ', '\t', '\n', '.', ',' };
+
+            foreach (string word in strings)
+            {
+                list.Add(word.Trim(trim_chars));
+            }
+
+            return list;
         }
     }
 }
