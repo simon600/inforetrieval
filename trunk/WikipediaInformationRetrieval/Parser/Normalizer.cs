1cda957fb8e38bb756cb00480dfa302efdda55ce
szymek.fogiel <szymek.fogiel@43718346-3035-11de-94d6-134fa40061eb>
2009-05-03 16:41:15 +0000

    Lots of changes.

---
+++ trunk/WikipediaInformationRetrieval/Parser/Normalizer.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Parser
+{
+    /// <summary>
+    /// Normalizes strings.
+    /// </summary>
+    public class Normalizer
+    {
+        /// <summary>
+        /// Default constructor.
+        /// </summary>
+        public Normalizer()
+        {
+            string str = ":-+=-_";
+
+            remove_chars = str.ToCharArray();
+        }
+
+        /// <summary>
+        /// Performs normalization.
+        /// </summary>
+        /// <param name="word">A string to normalize.</param>
+        /// <returns>Normalized string.</returns>
+        public string Normalize(string word)
+        {
+            string str = word;
+            foreach (char c in remove_chars)
+            {
+                word = word.Replace(c.ToString(),"");
+            }
+            return word.ToLower();
+        }
+
+        private char[] remove_chars;
+    }
+}
--- trunk/WikipediaInformationRetrieval/Parser/Tokenizer.cs
+++ trunk/WikipediaInformationRetrieval/Parser/Tokenizer.cs
@@ -14,22 +14,36 @@ namespace Parser
     public class Tokenizer
     {
         /// <summary>
+        /// Default constructor.
+        /// </summary>
+        public Tokenizer()
+        {
+            string str = " \t\n\r.,'\";:-_=+(){}[]!@#$%^&*|\\/><";
+            trim_chars = str.ToCharArray();
+
+            str = ".,;";
+
+            split_chars = str.ToCharArray();
+        }
+
+        /// <summary>
         /// Converts list of strings into another list of strings (tokens).
         /// </summary>
         /// <param name="strings">List of strings to convert</param>
         /// <returns>Converted list of strings</returns>
         public List<string> ConvertStrings(List<string> strings)
         {
-            List<string> list = new List<string>();
-            char[] trim_chars = { ' ', '\t', '\n', '\r', '.', ',', '(',
-                                  ')',  '[',  ']', '"', '\'', '/', '\\'};
-
+            List<string> list = new List<string>();         
+            
             foreach (string word in strings)
-            {
-                list.Add(word.Trim(trim_chars).ToLower());
+            {                
+                list.AddRange(word.Trim(trim_chars).Split(split_chars));
             }
 
             return list;
         }
+
+        private char[] trim_chars;
+        private char[] split_chars;
     }
 }
