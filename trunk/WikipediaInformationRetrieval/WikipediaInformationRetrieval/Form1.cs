c42f4952ef6090ecb346793397311d6b98025486
basia.szymanek <basia.szymanek@43718346-3035-11de-94d6-134fa40061eb>
2009-05-04 21:30:47 +0000

    Boolean query

--- trunk/WikipediaInformationRetrieval/WikipediaInformationRetrieval/Form1.cs
+++ trunk/WikipediaInformationRetrieval/WikipediaInformationRetrieval/Form1.cs
@@ -6,6 +6,10 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
+using System.IO;
+using WikipediaSearchEngine;
+using Morphologic;
+using InversedIndex;
 
 namespace WikipediaInformationRetrieval
 {
@@ -14,6 +18,37 @@ namespace WikipediaInformationRetrieval
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            FileStream f = new FileStream("D:\\ZAJECIA\\WyszukiwanieInformacji\\projekt1\\index2.bin", FileMode.Open);
+            InversedIndex.InversedPositionalIndex index = InversedIndex.InversedPositionalIndex.Get();
+            index.ReadFromStream(f);
+
+            f.Close();
+
+            int i = index.Documents.Count;
+            index.PostingList("dziesiąty");
+
+            MorphologicDictionary morphologic =
+                    MorphologicDictionary.Get();
+
+            morphologic.ReadFromFile("D:\\ZAJECIA\\WyszukiwanieInformacji\\projekt1\\morphologic.bin");
+            List<string> l = morphologic["dzisiąty"];
+            l = morphologic["astronomia"];
+
+           
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+            BooleanQuery query = new BooleanQuery(this.textBox1.Text);
+            this.textBox2.Text = query.QueryNormalForm;
+            PositionalPostingList posting = query.ProcessQuery(); 
+
+
         }
     }
 }
