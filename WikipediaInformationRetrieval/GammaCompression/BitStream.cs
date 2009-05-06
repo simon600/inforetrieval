cab3bd190c6ad00950c02de350196552046f0f61
basia.szymanek <basia.szymanek@43718346-3035-11de-94d6-134fa40061eb>
2009-05-06 10:21:12 +0000

    very simple class for bit stream representation and gamma conversion

---
+++ WikipediaInformationRetrieval/GammaCompression/BitStream.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GammaCompression
+{
+    public class BitStream
+    {
+        public BitStream()
+        {
+            mByteTape = new List<byte>();
+            mByteTape.Add(0);
+
+            mIndex = 0;
+            mMaskIndex = 0;
+        }
+
+        public BitStream(byte[] bytes)
+        {
+            mByteTape = new List<byte>(bytes);
+
+            mIndex = 0;
+            mMaskIndex = 0;
+        }
+
+        public void SetNextBit(bool value)
+        {
+            if (value)
+                mByteTape[mIndex] |= mMasks[mMaskIndex];
+
+            mMaskIndex++;
+            if(mMaskIndex == 8)
+            {
+                mMaskIndex = 0;
+                mIndex++;
+                mByteTape.Add(0);
+            }
+
+        }
+
+        /// <summary>
+        /// throw IndexOutOfBound Exception if its End of Stream
+        /// </summary>
+        /// <returns></returns>
+        public bool GetNextBit()
+        {
+            int result = mByteTape[mIndex] & mMasks[mMaskIndex];
+
+            mMaskIndex++;
+            if (mMaskIndex == 8)
+            {
+                mMaskIndex = 0;
+                mIndex++;
+            }
+
+            return result > 0;
+        }
+
+        public void SetOnStart()
+        {
+            mBitLength = (mIndex << 3) + mMaskIndex;
+
+            mIndex = 0;
+            mMaskIndex = 0;
+        }
+
+        public void GoToEnd()
+        {
+            mMaskIndex = mBitLength % 8;
+            mIndex = mBitLength >> 3;
+        }
+
+        public void ResetStream()
+        {
+            mIndex = 0;
+            mMaskIndex = 0;
+
+            mByteTape.Clear();
+            mByteTape.Add(0);
+        }
+
+        public int Length
+        {
+            get
+            {
+                int position = (mIndex << 3) + mMaskIndex;
+                if (position < mBitLength)
+                    return mBitLength;
+                else return position;
+            }
+        }
+
+        public int StreamSize
+        {
+            get
+            {
+                return mByteTape.Count;
+            }
+        }
+
+        public byte[] Bytes
+        {
+            get
+            {
+                return mByteTape.ToArray();
+            }
+        }
+
+        public bool EndOfStream
+        {
+            get
+            {
+                return Length == (mIndex << 3) + mMaskIndex; 
+            }
+        }
+
+        private List<byte> mByteTape;
+        private int mIndex;
+        private int mMaskIndex;
+        private int mBitLength;
+
+        private byte[] mMasks = { 128, 64, 32, 16, 8, 4, 2, 1 };
+    }
+}
---
+++ WikipediaInformationRetrieval/GammaCompression/GammaEncoding.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GammaCompression
+{
+    public class GammaEncoding
+    {
+        public static void CodeInt(uint value, BitStream stream)
+        {
+            string binary_value = Convert.ToString(value+1, 2);
+            
+            //unary code for length of binary_value
+            for (int i = 1; i < binary_value.Length; i++)
+                stream.SetNextBit(false);
+
+            for (int i = 0; i < binary_value.Length; i++)
+                stream.SetNextBit(binary_value[i] == '1');
+
+        }
+
+        public static uint DecodeInt(BitStream stream)
+        {
+            int len = 0;
+
+            while (stream.GetNextBit() == false)
+                len += 1;
+
+            string binary_value = "1";
+            
+            for (int i = 0; i < len; i++)
+                if (stream.GetNextBit())
+                    binary_value += '1';
+                else binary_value += '0';
+
+            return Convert.ToUInt32(binary_value, 2) -1;
+        }
+
+    }
+}
