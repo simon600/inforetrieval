361e73ad1134fa3fdfa524102c153875537abd04
basia.szymanek <basia.szymanek@43718346-3035-11de-94d6-134fa40061eb>
2009-05-06 10:21:12 +0000

    very simple class for bit stream representation and gamma conversion

---
+++ trunk/WikipediaInformationRetrieval/GammaCompression/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace GammaCompression
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            //Application.EnableVisualStyles();
+            //Application.SetCompatibleTextRenderingDefault(false);
+            //Application.Run(new Form1());
+
+            BitStream bitStream = new BitStream();
+
+            for (uint i = 0; i < 100; i++)
+            {
+
+                GammaEncoding.CodeInt(i, bitStream);
+                Console.WriteLine(bitStream.Length+" size "+bitStream.StreamSize);
+            }
+
+            bitStream.SetOnStart();
+           
+         
+
+            while (!bitStream.EndOfStream)
+            {
+                uint val = GammaEncoding.DecodeInt(bitStream);
+                Console.WriteLine(val);
+            }
+         
+        }
+    }
+}
